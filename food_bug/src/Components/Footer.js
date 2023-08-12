43b92bef012d91f8d1728f2cc39debd59b0bc1cc
TejasNayak42 <tejasnayakc42@gmail.com>
2023-08-12 19:24:58 +0530

    Footer Added

--- food_bug/src/Components/Footer.js
+++ food_bug/src/Components/Footer.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div
+      className="footer"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "Black",
+        color: "white",
+        height: "50px",
+      }}
+    >
+      Food Bug &nbsp;
+      <a href="https://github.com/TejasNayak42">@TejasNayak42</a>
+    </div>
+  );
 }
